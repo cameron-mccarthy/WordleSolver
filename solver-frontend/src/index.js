7c00f62de0b21d00678e5bd517f3dc8825411665
cameron-mccarthy <cameronsm21@gmail.com>
2025-03-21 22:27:43 -0500

    complexity ensues

---
+++ solver-frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
